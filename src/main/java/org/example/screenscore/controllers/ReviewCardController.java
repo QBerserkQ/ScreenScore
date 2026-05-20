2844710f8a1021cb633bad0574ac32f830a97d84
vova <vk4935391@gmail.com>
2026-05-20 12:52:54 +0300

    fix: deleted url field

--- src/main/java/org/example/screenscore/controllers/ReviewCardController.java
+++ src/main/java/org/example/screenscore/controllers/ReviewCardController.java
@@ -1,6 +1,5 @@
 package org.example.screenscore.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,10 +12,8 @@ import javafx.stage.Stage;
 import org.example.screenscore.models.ReviewClass;
 import org.example.screenscore.models.Type;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.function.Consumer;
 
 public class ReviewCardController {
@@ -87,26 +84,21 @@ public class ReviewCardController {
     }
 
     private void setUrlLabel(){
-        String url = review.getImageUrl();
+        String localPath = review.getImageUrl();
 
-        if (url != null && !url.isBlank()) {
-            try {
-                URL imageUrl = new URL(url);
-                HttpURLConnection connection = (HttpURLConnection) imageUrl.openConnection();
-
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-                InputStream inputStream = connection.getInputStream();
-                Image image = new Image(inputStream);
-
-                urlLabel.setImage(image);
-            } catch (Exception e) {
-                System.out.println("Bad image URL: " + url);
-                e.printStackTrace();
-            }
-        } else {
+        if(localPath == null || localPath.isEmpty()){
             urlLabel.setImage(null);
+            return;
         }
+
+        File file = new File(localPath);
+        if(!file.exists()){
+            urlLabel.setImage(null);
+            return;
+        }
+
+        Image image = new Image(file.toURI().toString());
+        urlLabel.setImage(image);
     }
 
     public void onDeleteButtonClicked() {
